521aee2ed3820d4deef8c5792195035ebc3a7105
Nagy Dávid <d1991d@gmail.com>
2024-03-02 18:23:42 +0100

    Egyedi 404 error oldal

---
+++ app/Views/error/404.php
@@ -0,0 +1,11 @@
+<div class="content clear-fix">
+
+    <ul class="no-list clear-fix section-list">
+
+        <li class="text-center clear-fix">
+            <h1>Oops! Page Not found</h1>
+        </li>
+
+    </ul>
+
+</div>
